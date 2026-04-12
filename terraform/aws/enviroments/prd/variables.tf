a93d81ea167d27501405edc15f7f6d4045b59f4c
r-fukuda <futianluren195@outlook.com>
2026-04-13 08:54:48 +0900

    20260413

---
+++ terraform/aws/enviroments/prd/variables.tf
@@ -0,0 +1,43 @@
+variable env {
+  type = string
+}
+
+variable project_name {
+  type = string
+}
+
+variable ec2_instance_count {
+  type = number
+}
+
+variable ec2_instance_type {
+  type = string
+}
+
+variable cidr_block_vpc {
+  type = string
+}
+
+variable source_cidr_blocks {
+  type = string
+}
+
+variable public_subnet_1a {
+  type = string
+}
+
+variable public_subnet_1c {
+  type = string
+}
+
+variable private_subnet_1a {
+  type = string
+}
+
+variable private_subnet_1c {
+  type = string
+}
+
+variable  route_cidr_block {
+  type = string
+}
