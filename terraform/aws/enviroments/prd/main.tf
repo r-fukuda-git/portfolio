9d863f0bc13fa2bc7c6db5364b7e3933f1fac6cd
r-fukuda <futianluren195@outlook.com>
2026-04-13 20:50:55 +0900

    20260413_01

--- terraform/aws/enviroments/prd/main.tf
+++ terraform/aws/enviroments/prd/main.tf
@@ -1,42 +1,42 @@
 provider "aws" {
-    region = "ap-northeast-1"
+  region = "ap-northeast-1"
 }
 
 module "networking" {
-    source = "../../modules/networking"
-    env = var.env
-    project_name = var.project_name
-    cidr_block_vpc = var.cidr_block_vpc
-    route_cidr_block = var.route_cidr_block
-    source_cidr_blocks = var.source_cidr_blocks
-    public_subnet_1a = var.public_subnet_1a
-    public_subnet_1c = var.public_subnet_1c
-    private_subnet_1a = var.private_subnet_1a
-    private_subnet_1c = var.private_subnet_1c
+  source             = "../../modules/networking"
+  env                = var.env
+  project_name       = var.project_name
+  cidr_block_vpc     = var.cidr_block_vpc
+  route_cidr_block   = var.route_cidr_block
+  source_cidr_blocks = var.source_cidr_blocks
+  public_subnet_1a   = var.public_subnet_1a
+  public_subnet_1c   = var.public_subnet_1c
+  private_subnet_1a  = var.private_subnet_1a
+  private_subnet_1c  = var.private_subnet_1c
 }
 
 module "security_group" {
-    source = "../../modules/sg"
-    vpc_id = module.networking.vpc_id
-    env = var.env
-    project_name = var.project_name
-    source_cidr_blocks = var.source_cidr_blocks
+  source             = "../../modules/sg"
+  vpc_id             = module.networking.vpc_id
+  env                = var.env
+  project_name       = var.project_name
+  source_cidr_blocks = var.source_cidr_blocks
 }
 
 module "ec2" {
-    source = "../../modules/ec2"
-    env = var.env
-    project_name = var.project_name
-    ec2_instance_count = var.ec2_instance_count
-    ec2_instance_type = var.ec2_instance_type
-    key_path = var.key_path
-    subnet_id = module.networking.subnet_public_id_1a
-    security_groups = [module.security_group.public_sg_id]
-    iam_instance_profile = module.iam.iam_instance_profile
+  source               = "../../modules/ec2"
+  env                  = var.env
+  project_name         = var.project_name
+  ec2_instance_count   = var.ec2_instance_count
+  ec2_instance_type    = var.ec2_instance_type
+  key_path             = var.key_path
+  subnet_id            = module.networking.subnet_public_id_1a
+  security_groups      = [module.security_group.public_sg_id]
+  iam_instance_profile = module.iam.iam_instance_profile
 }
 
-module "iam" { 
-    source = "../../modules/iam"
-    env = var.env
-    project_name = var.project_name
+module "iam" {
+  source       = "../../modules/iam"
+  env          = var.env
+  project_name = var.project_name
 }
--- terraform/aws/enviroments/prd/variables.tf
+++ terraform/aws/enviroments/prd/variables.tf
@@ -1,44 +1,44 @@
-variable env {
+variable "env" {
   type = string
 }
 
-variable project_name {
+variable "project_name" {
   type = string
 }
 
-variable ec2_instance_count {
+variable "ec2_instance_count" {
   type = number
 }
 
-variable ec2_instance_type {
+variable "ec2_instance_type" {
   type = string
 }
 
-variable cidr_block_vpc {
+variable "cidr_block_vpc" {
   type = string
 }
 
-variable source_cidr_blocks {
+variable "source_cidr_blocks" {
   type = string
 }
 
-variable public_subnet_1a {
+variable "public_subnet_1a" {
   type = string
 }
 
-variable public_subnet_1c {
+variable "public_subnet_1c" {
   type = string
 }
 
-variable private_subnet_1a {
+variable "private_subnet_1a" {
   type = string
 }
 
-variable private_subnet_1c {
+variable "private_subnet_1c" {
   type = string
 }
 
-variable  route_cidr_block {
+variable "route_cidr_block" {
   type = string
 }
 
