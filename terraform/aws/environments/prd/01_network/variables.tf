3877ea27821408b6d7fdbde2749fec7c6cf2183a
r-fukuda <futianluren195@outlook.com>
2026-04-27 21:31:58 +0900

    [update] root分割

---
+++ terraform/aws/environments/prd/01_network/variables.tf
@@ -0,0 +1,39 @@
+variable "env" {
+  type = string
+}
+
+variable "project_name" {
+  type = string
+}
+
+variable "cidr_block_vpc" {
+  type = string
+}
+
+variable "source_cidr_blocks" {
+  type = list(string)
+}
+
+variable "target_cidr_blocks" {
+  type = list(string)
+}
+
+variable "public_subnet_1a" {
+  type = string
+}
+
+variable "public_subnet_1c" {
+  type = string
+}
+
+variable "private_subnet_1a" {
+  type = string
+}
+
+variable "private_subnet_1c" {
+  type = string
+}
+
+variable "route_cidr_block" {
+  type = string
+}
