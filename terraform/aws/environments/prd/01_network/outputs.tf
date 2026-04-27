39952d5db48626f0ce80565e6b70f3a47682ee8b
r-fukuda <futianluren195@outlook.com>
2026-04-27 22:18:34 +0900

    [update] outputs.tfを追加

---
+++ terraform/aws/environments/prd/01_network/outputs.tf
@@ -0,0 +1,11 @@
+output "vpc_id" {
+  value = module.networking.vpc_id
+}
+
+output "subnet_public_id_1a" {
+  value = module.networking.subnet_public_id_1a
+}
+
+output "subnet_public_id_1c" {
+  value = module.networking.subnet_public_id_1c
+}
